8edbe46d4d752c0d2fe926131a9bfe5aafbf02db
Issa Olinga <issahermann@jepeuxlefaire.net>
2020-12-22 20:14:17 +0100

    les devoirs sur eds lecons10 et eds avec php

---
+++ eds_en_php/acceuil.inc.php
@@ -0,0 +1,17 @@
+<?php
+    echo "<h1> Lecture de la semaine: </h1>";
+    echo "<h2> Verset à mémoriser <br> « Les cieux racontent la gloire de Dieu,
+    l’étendue manifeste l’œuvre de ses mains » (Ps 19.1,). </h2>";
+    echo "<h4> L’éducation comprend ce que l’on a appelé « les arts et les sciences ». Mais lorsque nous apprenons ou enseignons les arts et les sciences dans une perspective biblique, qu’est-ce que cela implique? Est-ce que nous proposons simplement des versets bibliques choisis qui se rapportent à un aspect particulier de la médecine moderne ou de l’histoire de l’art, par exemple? Ce faisant, nous pouvons relier nos leçons pratiques à l’étonnante puissance de Dieu dans la création de notre monde complexe. 
+    Mais une simple incorporation de l’Écriture dans une leçon de manuel n’est qu’une petite partie 
+    de la véritable éducation, une éducation qui est salvatrice et rédemptrice.</h4>";
+    echo "<h4> Pour qu’une telle éducation fonctionne vraiment, nous avons besoin que la 
+    Parole de Dieu éclaire l’enseignement de chaque discipline, des sciences humaines à la biologie moléculaire.
+     Sans elle, nous pouvons perdre de vue l’immensité de Dieu, Sa souveraineté en tant que Créateur et Soutien de
+      notre monde. En apprenant à voir comment Dieu considère Sa création comme organique et remplie de desseins, 
+      nous nous rapprochons de la compréhension de la manière dont certaines disciplines pourraient
+     et devraient être enseignées. </h4>";
+    echo "<h4> Cette semaine, nous examinerons certains principes qui entrent en jeu dans 
+    la manière dont nous enseigner les arts et les sciences 
+    dans une perspective et une vision chrétiennes du monde. </h4>";
+?>
