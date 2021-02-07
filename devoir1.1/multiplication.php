4c1fea676ce152afc76f9732e3c16ecb4dc517fd
Issaolinga <75489209+Issaolinga@users.noreply.github.com>
2021-02-07 16:55:57 +0100

    Add files via upload

--- devoir1.1/multiplication.php
+++ devoir1.1/multiplication.php
@@ -1,29 +1,28 @@
-<?php
-    echo "<h2> Révisez votre table de multiplication!</ h2>";
-    //Début du tableau HTML
-    echo "<table border=\"2\" style=\"background-color:yellow\"> <th>
-    &nbsp;X &nbsp;</th>";
-    //Création de la première ligne
-    for($i=1;$i<10;$i++)
-    {
-        echo "<th>&nbsp;$i&nbsp;</th>";
-    }
-    //Fin de la boucle 1
-    //*****************************
-    //Création du corps de la table
-    //*****************************
-    //Boucles de création du contenu de la table
-    for($i=1;$i<10;$i++)
-    {    
-        //Création de la première colonne  
-        echo "<tr><th>&nbsp;$i&nbsp;</th>";  
-        //Remplissage de la table  
-        for($j=1;$j<10;$j++)
-        {    
-            echo "<td style=\"background-color:red;color:white\"> &nbsp;&nbsp; <b>". $i*$j.
-            "&nbsp;&nbsp; </td>";  
-        }  
-        echo "</b></tr>";
-        }
-    echo "</table>"
-?>
+<!DOCTYPE html> 
+  
+
+  <?php
+	echo "<center><h1><b>TABLE DE MULTIPLICATION</h1></b>";
+  ?>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+          Entrer un nombre: <input type="integer" name="Nombre">
+          <br><br>
+          <input type="submit" name="Calcule" value="Calcule"> 
+    </form>
+    <br/> 
+
+     
+	<?php
+
+	 $j=$_POST['Nombre'];
+	 for ($i=1; $i<=14; $i++)
+
+	 {
+	     echo $i." x ".$j."	= ".$i*$j."<br>";
+	 }
+
+	?>
+    
+
+</body>
+</html>
